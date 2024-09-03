72a25f20c5e6ea4bd7750f38f9db365d43db1fef
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-09-03 19:46:22 +0300

    day013

---
+++ 09-Map Collections, Package and Import, Static and Final, Enums/Map Collections/City Population/CityPopulationTracker.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class CityPopulationTracker {
+    
+    private HashMap<String, City> cityPopulation;
+
+    public CityPopulationTracker() {
+        this.cityPopulation = new HashMap<>();
+    }
+
+    public City getCity(String cityName) {
+        return new City(cityPopulation.get(cityName));
+    }
+
+    public void setCity(City city) {
+        cityPopulation.put(city.getName(), new City(city));
+    }
+
+    public void addCity(City city) {
+        cityPopulation.put(city.getName(), new City(city));
+    }
+}
